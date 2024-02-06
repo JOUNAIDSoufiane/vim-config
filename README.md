e43a1e520e54258b8866087e5804d7f567ce6e34
Soufiane Jounaid <soufianej@Soufianes-UC-Macbook-Pro.local>
2024-02-06 11:19:22 -0600

    Minimal vim setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vim-config
 Repository that holds my vimrc and some bash/zsh configurations 
+
+# Installation
+run ```bash setup.sh -o {name of os, either macos or linux}```
---
+++ setup.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Default value for the os flag
+os=""
+
+# Parse command-line arguments
+while getopts ":o:" opt; do
+  case $opt in
+    o)
+      os="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+# Check if operating system flag is provided
+if [ -z "$os" ]; then
+    echo "Error: Operating system flag (-o) not provided"
+    exit 1
+fi
+
+# Function to install Neovim on macOS
+install_neovim_macos() {
+    brew install neovim
+}
+
+# Function to install Neovim on Linux
+install_neovim_linux() {
+    sudo apt-get update
+    sudo apt install neovim
+}
+
+# Install Neovim based on the specified operating system
+case "$os" in
+    "macos")
+        install_neovim_macos
+        ;;
+    "linux")
+        install_neovim_linux
+        ;;
+    *)
+        echo "Unsupported operating system"
+        exit 1
+        ;;
+esac
+
+# Create Neovim configuration directory if it doesn't exist
+mkdir -p ~/.config/nvim/
+
+# Copy Neovim configuration
+cp -r init.vim ~/.config/nvim/
+
+# Instal Vim-Plug (if not already installed)
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Install plugins using Vim-Plug
+nvim +PlugInstall +qall
